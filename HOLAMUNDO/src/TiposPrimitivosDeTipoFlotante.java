f6ccf7174fab15ffb3e8006fe69738246eb64052
Freyder ¨Inked¨ Urbano <freurbano@hotmail.com>
2024-01-17 12:46:49 -0500

    commit

---
+++ HOLAMUNDO/src/TiposPrimitivosDeTipoFlotante.java
@@ -0,0 +1,14 @@
+
+public class TiposPrimitivosDeTipoFlotante {
+    public static void main(String args[]){
+        
+        //varias formas para definir un numero de tipo flotante
+        float numeroFloat = 10.0f;
+        float numFloat = (float)10.0;
+        
+        System.out.println(numeroFloat);
+        System.out.println("MINVALUE: " + Float.MIN_VALUE);
+        System.out.println("MAXVALUE: " + Float.MAX_VALUE);
+        
+    }
+}
